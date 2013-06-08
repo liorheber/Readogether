808f5334bdb48a63556779d07d6a37447eeda081
Lior Heber <lior.heber@kenshoo.com>
2013-06-09 01:01:52 +0300

    Lated version

--- js/client.js
+++ js/client.js
@@ -1,54 +1,52 @@
-$(document).ready(function() {  
-    
-    // Will be 0, for testing purposes is 2
-    var currentWord = 2;
-    
-    // Disable scrolling so the touch gestures will work good
-    document.ontouchstart = function(e){ 
-        e.preventDefault(); 
-    }
-    document.ontouchmove = function(e){ 
-        e.preventDefault(); 
-    }
-    
-    // The swipe on word effect that will call the server with an update on the current word
-    $('.text').hammer().on('touch', function(){
-        // Add Check if user is the reader
-        
-        // Save the current word that is being pressed
-        currentWord = $(this).data('message');
-        
-        // Send to server the new current word
-        $.ajax({
-            type: "POST",
-            url: "Server.php",
-            data: currentWord,
-        })
-        
-        // Color the word with animation
-        $(this).animate({'color': '#6633FF'}, 1000);
-    });
-    
-    function updateCurrentWord() {
-        $.ajax({
-            type: "POST",
-            url: "Server.php",
-            success: function(data){
-                currentWord = data;
-                // The coloring effect when getting a response from the server on the current word
-                $('.text').each(function() {
-                    // Check if this is a word already been read, if so color it with animation
-                    if ($(this).data('message') < currentWord) {
-                        $(this).animate({'color': '#6633FF'}, 1000);
-                    }
-                });
-                setTimeout(updateCurrentWord, 1000);
-            },
-            error: function(){
-                setTimeout(updateCurrentWord, 1000);
+function getBook() {
+    $.ajax({
+        type: "GET",
+        url: "bin/Server.php?action=getText",
+        dataType: "json",
+        success: function(data) {
+            text = data;
+            var i;
+            var j;
+            var t = 0;
+            for (i = 0; i < text.length; i++) {
+                for (j = 0; j < text[i].length; j++) {
+                    $("#book").append('<a class="text" data-message="' + t + '">');
+                    $("#book").append(text[i][j]);
+                    $("#book").append(" </a>");
+                    t++;
+                }
+                $("#book").append("<br />");
+                $("#log-in").css("display", "none");
             }
-        })
-    };
-    
+        }
+    });
+}
 
-});
+function getCurrentWord() {
+    $.ajax({
+        type: "GET",
+        url: "bin/Server.php?action=currentWord",
+        dataType: "json",
+        success: function(data) {
+            currentWord = data;
+        }
+    });
+}
+
+function addUser() {
+    var name = $("#name").val();
+    if (name === "") {
+        alert("אנא הכנס שם");
+    } else {
+        $.ajax({
+            type: "GET",
+            url: "bin/Server.php?action=connect&name=" + name,
+            dataType: "json",
+            success: function(data) {
+                userList = data;
+                getBook();
+                getCurrentWord();
+            }
+        });
+    }
+}
